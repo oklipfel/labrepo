96e10da7376b95d654f47f1fecce9b2bf82613a8
Michael Ross <mross@students.pitzer.edu>
2023-02-03 11:52:49 -0800

    fixed the fixme, passed test cases

--- Fixme.py
+++ Fixme.py
@@ -4,3 +4,8 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
+    xs = range(0,n+1)
+    xs = filter(lambda x: x % 2 == 0, xs)
+    xs = list(xs)
+    return xs
+
